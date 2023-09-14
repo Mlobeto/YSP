834d95130cd45b12f6462ff2ef19f05f885cb9a3
Maria Lobeto <111536207+Mlobeto@users.noreply.github.com>
2023-09-13 21:46:53 -0300

    Update FirebaseConfig.js

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -3,8 +3,7 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuykRasFoN0P9vmaQFuh7-A_c5FZd0qL0",
-  authDomain: "nysp-f04e4.firebaseapp.com",
+//////////////////////////////
   projectId: "nysp-f04e4",
   storageBucket: "nysp-f04e4.appspot.com",
   messagingSenderId: "522119906082",
