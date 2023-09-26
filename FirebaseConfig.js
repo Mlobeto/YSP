6bae4b167d43fc3874850c403e88e425c3f901db
Maria Lobeto <111536207+Mlobeto@users.noreply.github.com>
2023-09-26 14:56:07 -0300

    Update FirebaseConfig.js

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -4,10 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 //////////////////////////////
-  projectId: "nysp-f04e4",
-  storageBucket: "nysp-f04e4.appspot.com",
-  messagingSenderId: "522119906082",
-  appId: "1:522119906082:web:3a8a9c2de8fca45ebb5b7b"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
